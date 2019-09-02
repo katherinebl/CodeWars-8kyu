9ab12afcca66f454d503c067e86400dc3b49794e
katherinebl <katherine.briceno@kairosds.com>
2019-09-02 15:21:56 +0200

    Add another solution for 'Fake Binary' kata

--- Fake Binary.js
+++ Fake Binary.js
@@ -1,5 +1,13 @@
 //Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
+// function fakeBin(x){
+//     let result = '';
+//     for(let i = 0; i < x.length; i++){
+//       x[i] < 5 ? result += 0 : result += 1;
+//     }
+//     return result;
+// }
+
 function fakeBin(x) {
     return x.split('').map(i => i >= 5 ? 1 : 0).join('');
 };
